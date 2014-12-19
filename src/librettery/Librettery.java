d3ba0d3b9edae4fd21186ca0da2ee5a96d56d94b
Adrien Pouyet <adrien@pouyet.fr>
2014-12-19 18:48:30 +0100

    make static declaration of libreterry class

--- src/librettery/Librettery.java
+++ src/librettery/Librettery.java
@@ -1,9 +1,21 @@
 package librettery;
 
-public class Librettery {
+import java.util.Map;
+import java.util.TreeMap;
 
-	public Librettery() {
-		// TODO Auto-generated constructor stub
+public class Librettery {
+	private static Map<Integer, Abonne> abonnes = new TreeMap<Integer, Abonne>();
+	private static Map<Integer, Document> documents = new TreeMap<Integer, Document>();
+	
+	public static void reserver(int idAbo, int idDoc){
+		
+	}
+	
+	public static void emprunter(int idAbo, int idDoc){
+		
 	}
 
+	public static void retour(int idAbo, int idDoc){
+	
+	}
 }
