89d9e36f4a6aedc28aeada8b6caf4dc1b473b8ef
BriocheDu78 <kcierreip.atafa@gmail.com>
2014-12-19 12:25:31 +0100

    start dev Livre class (//TODO Timer)

---
+++ src/librettery/Livre.java
@@ -0,0 +1,34 @@
+package librettery;
+
+import java.util.Timer;
+
+public class Livre implements Document {
+	public int numero;
+	public Abonne abo;
+
+	@Override
+	public int numero() {
+		return this.numero;
+	}
+
+	@Override
+	public void reserver(Abonne ab) throws PasLibreException {
+		if (this.abo == null)
+			throw new PasLibreException();
+		this.abo = ab;
+		
+	}
+
+	@Override
+	public void emprunter(Abonne ab) throws PasLibreException {
+		if (this.abo == null)
+			throw new PasLibreException();
+		this.abo = ab;
+	}
+
+	@Override
+	public void rendreDispo() {
+		this.abo = null;
+	}
+
+}
