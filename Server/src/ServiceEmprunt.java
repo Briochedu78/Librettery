407871bff77330ea11af6170d9d4bbcde6dffa41
Adrien Pouyet <adrien@pouyet.fr>
2014-12-19 18:23:54 +0100

    Create all the servics

---
+++ Server/src/ServiceEmprunt.java
@@ -0,0 +1,21 @@
+import java.net.Socket;
+
+
+public class ServiceEmprunt implements Runnable {
+
+	public ServiceEmprunt(Socket accept) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void lancer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Server/src/ServiceReservation.java
@@ -0,0 +1,21 @@
+import java.net.Socket;
+
+
+public class ServiceReservation implements Runnable {
+
+	public ServiceReservation(Socket accept) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void lancer() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
