679869c114418f3bcd6b72e4e174541fd3a86a8f
Adrien Pouyet <adrien@pouyet.fr>
2014-12-19 11:58:16 +0100

    start coding Abonne and Reservation

--- src/librettery/Abonne.java
+++ src/librettery/Abonne.java
@@ -1,5 +1,27 @@
 package librettery;
 
 public class Abonne {
+	private int numero;
+	private String nom;
+	
+	
+	public Abonne(String nom){
+		this.numero = Abonne.generateNumero();
+		this.nom = nom;
+	}
 
+
+	private static int generateNumero() {
+		return (int) (Math.random() * Integer.MAX_VALUE);
+	}
+
+
+	public int getNumero() {
+		return numero;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
 }
--- src/librettery/Reservation.java
+++ src/librettery/Reservation.java
@@ -1,5 +1,30 @@
 package librettery;
 
-public class Reservation {
+import java.util.Timer;
 
+public class Reservation {
+	private int idReservation;
+	private int idAbo;
+	private int idDocument;
+	private FinReservation tempRestant;
+	
+	/**
+	 * 1h = 1000ms
+	 */
+	private static int tempsReservation = 5000;
+	
+	public Reservation(Abonne abo, Document document){
+		this.idAbo = abo.getNumero();
+		this.idDocument = document.numero();
+		this.idReservation = Reservation.generateNumero();
+	}
+
+	private static Timer createTimer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private static int generateNumero() {
+		return (int) (Math.random() * Integer.MAX_VALUE);
+	}
 }
