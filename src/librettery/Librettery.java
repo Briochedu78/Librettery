6a5a0e4ad38bad720354972d11ce4a06df79a39f
Ricocotam <adrien@pouyet.fr>
2014-12-26 20:44:42 +0100

    code reserver, emprunter, retour of Librettery

--- src/librettery/Librettery.java
+++ src/librettery/Librettery.java
@@ -6,16 +6,36 @@ import java.util.TreeMap;
 public class Librettery {
 	private static Map<Integer, Abonne> abonnes = new TreeMap<Integer, Abonne>();
 	private static Map<Integer, Document> documents = new TreeMap<Integer, Document>();
-	
-	public static void reserver(int idAbo, int idDoc){
-		
-	}
-	
-	public static void emprunter(int idAbo, int idDoc){
-		
+
+	public static void reserver(int idAbo, int idDoc) throws PasLibreException {
+		if (documents.containsKey(idDoc) && abonnes.containsKey(idAbo)) {
+			synchronized (documents.get(idDoc)) {
+				documents.get(idDoc).reserver(abonnes.get(idAbo));
+			}
+		} else {
+			throw new IllegalArgumentException("L'abonne " + idAbo
+					+ " n'existe pas ou le document " + idDoc + " n'existe pas");
+		}
 	}
 
-	public static void retour(int idAbo, int idDoc){
-	
+	public static void emprunter(int idAbo, int idDoc) throws PasLibreException {
+		if (documents.containsKey(idDoc) && abonnes.containsKey(idAbo)) {
+			synchronized (documents.get(idDoc)) {
+				documents.get(idDoc).emprunter(abonnes.get(idAbo));
+			}
+		} else {
+			throw new IllegalArgumentException("L'abonne " + idAbo
+					+ " n'existe pas ou le document " + idDoc + " n'existe pas");
+		}
+	}
+
+	public static void retour(int idDoc) throws PasLibreException {
+		if (documents.containsKey(idDoc)) {
+			synchronized (documents.get(idDoc)) {
+				documents.get(idDoc).rendreDispo();
+			}
+		} else {
+			throw new IllegalArgumentException("Le document " + idDoc + " n'existe pas");
+		}
 	}
 }
