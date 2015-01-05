e6bfece75319f22a26733f29d02c4f2b3904cfcc
Ricocotam <adrien@pouyet.fr>
2015-01-05 21:31:56 +0100

    update exception

--- src/librettery/PasLibreException.java
+++ src/librettery/PasLibreException.java
@@ -3,7 +3,7 @@ package librettery;
 @SuppressWarnings("serial")
 public class PasLibreException extends Exception {
 	public PasLibreException (String string) {
-		super(string);
+		super("Le document n'est pas libre" + string);
 	}
 	
 	public PasLibreException(){
