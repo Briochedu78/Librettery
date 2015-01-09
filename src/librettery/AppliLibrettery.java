1b007a29e270ad26dcc8da531fbcd266470efd43
Adrien Pouyet <adrien@pouyet.fr>
2015-01-09 11:13:51 +0100

    chèvre

---
+++ src/librettery/AppliLibrettery.java
@@ -0,0 +1,18 @@
+package librettery;
+
+public class AppliLibrettery {
+	
+	private static int facteur_jour = 1000;
+	
+	public static int getMilliNbJours(int nbjours){
+		return nbjours * facteur_jour;
+	}
+	
+	public static void add() {
+		String type = "li";
+		for(int i = 0; i < 10; ++i){
+			Librettery.ajouter(FDocument.buildDoc(type + (i + 1)));
+			Librettery.ajouter(new Abonne("toto" + (i + 1), (int) (Math.random() * 80)));
+		}
+	}
+}
