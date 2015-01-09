7e148008c8978a6a85fba96f573bc3c72b0bccb6
Adrien Pouyet <adrien@pouyet.fr>
2015-01-09 10:53:44 +0100

    add generation of documents

--- src/librettery/Appli.java
+++ src/librettery/Appli.java
@@ -7,4 +7,12 @@ public class Appli {
 	public static int getMilliNbJours(int nbjours){
 		return nbjours * facteur_jour;
 	}
+	
+	public static void main(String[] args) {
+		String type = "li";
+		for(int i = 0; i < 10; ++i){
+			Librettery.ajouter(FDocument.buildDoc(type + (i + 1)));
+			Librettery.ajouter(new Abonne("toto" + (i + 1), (int) (Math.random() * 80)));
+		}
+	}
 }
